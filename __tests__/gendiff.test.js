94681024ddf3da2f1e46f104700deb490b867c3d
ian proletov <yan.proletov@gmail.com>
2022-08-02 17:19:31 +0300

    add readFile and getFixturePath

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,12 @@
 import fs from 'fs';
+import path from 'path';
 import gendiff from '../index.js';
 
+const getFixturePath = (filename) => path.join('__tests__/__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
 test('plain json', () => {
-  const actual = gendiff('__tests__/__fixtures__/file1.json', '__tests__/__fixtures__/file2.json');
-  const expected = fs.readFileSync('__tests__/__fixtures__/resjson', 'utf-8').trim();
+  const actual = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  const expected = readFile('resjson').trim();
   expect(actual).toBe(expected);
 });
