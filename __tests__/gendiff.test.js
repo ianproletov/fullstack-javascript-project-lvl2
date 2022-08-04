b80f847c45e6c98c252933de6af7bf4583360a09
ian proletov <yan.proletov@gmail.com>
2022-08-04 15:16:57 +0300

    add yml tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,8 +5,12 @@ import gendiff from '../index.js';
 const getFixturePath = (filename) => path.join('__tests__/__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('plain json', () => {
-  const actual = gendiff(getFixturePath('before.json'), getFixturePath('after.json'));
-  const expected = readFile('diffjson').trim();
-  expect(actual).toBe(expected);
+describe('plain json', () => {
+  it.each(['json', 'yml'])('%p diff test', (ext) => {
+    const beforeName = `before.${ext}`;
+    const afterName = `after.${ext}`;
+    const actual = gendiff(getFixturePath(beforeName), getFixturePath(afterName));
+    const expected = readFile('diffjson').trim();
+    expect(actual).toBe(expected);
+  });
 });
