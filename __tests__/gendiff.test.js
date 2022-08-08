c2ff7c4fa6e301fe0cdfc65e2ea683401d99bac8
ian proletov <yan.proletov@gmail.com>
2022-08-08 22:48:43 +0300

    add default test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,12 +5,12 @@ import gendiff from '../index.js';
 const getFixturePath = (filename) => path.join('__tests__/__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe.each(['stylish', 'plain'])('%p format', (format) => {
+describe.each(['default', 'stylish', 'plain'])('%p format', (format) => {
   it.each(['json', 'yml'])('%p diff test', (ext) => {
     const beforeName = `before.${ext}`;
     const afterName = `after.${ext}`;
-    const actual = gendiff(getFixturePath(beforeName), getFixturePath(afterName), format);
-    const diffname = `${format}diff`;
+    const actual = gendiff(getFixturePath(beforeName), getFixturePath(afterName), format === 'default' ? undefined : format);
+    const diffname = `${format === 'default' ? 'stylish' : format}diff`;
     const expected = readFile(diffname).trim();
     expect(actual).toBe(expected);
   });
