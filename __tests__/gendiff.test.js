accf043885ba4e95f9890606c99dc0c495f8c399
ian proletov <yan.proletov@gmail.com>
2022-08-08 21:16:29 +0300

    add plain test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,12 +5,13 @@ import gendiff from '../index.js';
 const getFixturePath = (filename) => path.join('__tests__/__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('plain json', () => {
+describe.each(['stylish', 'plain'])('%p format', (format) => {
   it.each(['json', 'yml'])('%p diff test', (ext) => {
     const beforeName = `before.${ext}`;
     const afterName = `after.${ext}`;
-    const actual = gendiff(getFixturePath(beforeName), getFixturePath(afterName));
-    const expected = readFile('diffjson').trim();
+    const actual = gendiff(getFixturePath(beforeName), getFixturePath(afterName), format);
+    const diffname = `${format}diff`;
+    const expected = readFile(diffname).trim();
     expect(actual).toBe(expected);
   });
 });
